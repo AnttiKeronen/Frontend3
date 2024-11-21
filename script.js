48976f7d69efd7748be084bce21c1fa2b9e409d3
AnttiKeronen <antti.keronen@student.lut.fi>
2024-11-21 15:12:28 +0200

    juu

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const faqContainer = document.querySelector('.faq-content');
+  if (faqContainer) {
+    faqContainer.addEventListener('click', (e) => {
+      const groupHeader = e.target.closest('.faq-group-header');
+      if (!groupHeader) return;
+
+      const group = groupHeader.parentElement;
+      const groupBody = group.querySelector('.faq-group-body');
+      const icon = groupHeader.querySelector('i');
+      icon.classList.toggle('fa-plus');
+      icon.classList.toggle('fa-minus');
+      groupBody.classList.toggle('open');
+
+      const otherGroups = faqContainer.querySelectorAll('.faq-group');
+      otherGroups.forEach((otherGroup) => {
+        if (otherGroup !== group) {
+          const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+          const otherIcon = otherGroup.querySelector('.faq-group-header i');
+          otherGroupBody.classList.remove('open');
+          otherIcon.classList.remove('fa-minus');
+          otherIcon.classList.add('fa-plus');
+        }
+      });
+    });
+  }
+
+  
+  const hamburgerButton = document.querySelector('.hamburger-button');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburgerButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+
+  const myStudiesLink = document.getElementById('myStudiesLink');
+  const outsideSoftwareLink = document.getElementById('outsideSoftwareLink');
+  const contactInfoLink = document.getElementById('contactInfoLink');
+
+
+  myStudiesLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'me.html';
+  });
+  outsideSoftwareLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'moreaboutme.html';
+  });
+  contactInfoLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.footer').scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
